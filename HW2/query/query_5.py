a65dadbe2f41d76f0bd521dcf7e536583985a5f2
LM1997610 <115407939+LM1997610@users.noreply.github.com>
2024-05-24 12:01:15 +0200

    Add files via upload

---
+++ HW2/query/query_5.py
@@ -0,0 +1,41 @@
+
+import time
+import pandas as pd
+
+from tabulate import tabulate
+from pymongo import MongoClient
+
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['soccer_db']
+
+pipeline = [
+
+    {"$match": {"$or": [{"player_name": "Lionel Messi"},
+                         {"player_name": "Cristiano Ronaldo"}]}},
+
+    {"$lookup": {"from": "player_attributes",
+                 "localField": "player_api_id",
+                 "foreignField": "player_api_id",
+                 "as": "attributes"}},
+
+    {"$unwind": "$attributes"},
+
+    {"$group": {"_id": "$player_name",
+                "avg_rating": {"$avg": "$attributes.overall_rating"},
+                "avg_potential": {"$avg": "$attributes.potential"},
+                "avg_crossing": {"$avg": "$attributes.crossing"},
+                "avg_finishing": {"$avg": "$attributes.finishing"}}}    ]
+
+
+start_time = time.time()
+
+result = db.players.aggregate(pipeline)
+
+df = pd.DataFrame.from_records(result) 
+
+print()
+print(tabulate(df, headers='keys', tablefmt="github", numalign='center', stralign="center"))
+
+print(f"\n Execution time: {time.time() - start_time:.3f} seconds")
+
---
+++ HW2/query/query_6.py
@@ -0,0 +1,74 @@
+
+import time
+import pandas as pd
+
+from tabulate import tabulate
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['soccer_db']
+
+pipeline = [
+
+    {"$match": {"$expr": {"$eq": ["$home_team_goal", "$away_team_goal"]}}   },
+
+    {"$facet": {"home_draws": [{"$group": {"_id": "$home_team_api_id", "num_draws": {"$sum": 1}    }   }   ],
+                "away_draws": [{"$group": {"_id": "$away_team_api_id", "num_draws": {"$sum": 1}    }   }   ],   }   },
+
+    {"$project": {"draw_matches": {"$setUnion": ["$home_draws", "$away_draws"]}}   },
+
+    {"$unwind": "$draw_matches"},
+
+    {"$group": {"_id": "$draw_matches._id", "draws": {"$sum": "$draw_matches.num_draws"}    }   },
+    
+    {"$unionWith": {"coll": "matches",
+                    "pipeline": [{"$match": {"$expr": {"$gt": ["$home_team_goal", "$away_team_goal"]}}},
+                                 {"$group": {"_id": "$home_team_api_id", "num_wins": {"$sum": 1}}}      ]   }   },
+
+    {"$unionWith": {"coll": "matches",
+                    "pipeline": [{"$match": {"$expr": {"$lt": ["$home_team_goal", "$away_team_goal"]}}},
+                                 {"$group": {"_id": "$away_team_api_id", "num_wins": {"$sum": 1}}}      ]   }   },
+
+    {"$group": {"_id": "$_id",
+                "draws": {"$sum": "$draws"},
+                "wins": {"$sum": "$num_wins"}   }   },
+    
+    {"$unionWith": {"coll": "matches",
+                    "pipeline": [{"$match": {"$expr": {"$lt": ["$home_team_goal", "$away_team_goal"]}}},
+                                 {"$group": {"_id": "$home_team_api_id", "num_loses": {"$sum": 1}}}     ]   }   },
+    {"$unionWith": {"coll": "matches",
+                    "pipeline": [{"$match": {"$expr": {"$gt": ["$home_team_goal", "$away_team_goal"]}}},
+                                 {"$group": {"_id": "$away_team_api_id", "num_loses": {"$sum": 1}}}     ]   }   },
+
+    {"$group": {"_id": "$_id",
+                "draws": {"$sum": "$draws"},
+                "wins": {"$sum": "$wins"},
+                "loses": {"$sum": "$num_loses"}}    },
+    
+    {"$lookup": {"from": "teams",
+                 "localField": "_id",
+                 "foreignField": "team_api_id",
+                 "as": "team"}  },
+
+    {"$unwind": "$team" },
+    
+    {"$project": {"_id": 0,
+                  "team_long_name": "$team.team_long_name",
+                  "points": {"$add": [{"$multiply": ["$wins", 3]}, "$draws"]}   }   },
+    
+    {"$sort": {"points": -1}    },
+    {"$limit": 10   }
+
+]
+
+
+start_time = time.time()
+
+result = list(db.matches.aggregate(pipeline))
+
+df = pd.DataFrame.from_records(result) 
+
+print()
+print(tabulate(df, headers='keys', tablefmt="github", numalign='center', stralign="center"))
+
+print(f"\n Execution time: {time.time() - start_time:.3f} seconds")
